9251222cb0431528ea39771096be8e640a126ad4
nikennovirasari <nikennovirasari@gmail.com>
2022-10-20 15:25:51 +0700

    qr berlogo

--- application/views/Admin/v_qrac.php
+++ application/views/Admin/v_qrac.php
@@ -36,76 +36,76 @@
                                             foreach ($data_qrac as $hasil) :
                                             ?>
                                              <?php
-                                                if ($hasil['kode'] != NULL) {
-                                                    $file_name = str_replace('/', '_', $hasil['nomor_perkara']);
-                                                    $file_name = str_replace('.', '', $file_name);
-
-                                                    $params['data'] = 'https://datacenter.pa-bojonegoro.go.id:8089/keaslian/akta/asli/' . $hasil['kode'];
-                                                    $params['level'] = 'H';
-                                                    $params['size'] = 10;
-                                                    $params['savename'] = FCPATH . $file_name . '.png';
-                                                    $this->ciqrcode->generate($params);
-                                                }
-                                                ?>
-                                             <?php
                                                 // if ($hasil['kode'] != NULL) {
                                                 //     $file_name = str_replace('/', '_', $hasil['nomor_perkara']);
                                                 //     $file_name = str_replace('.', '', $file_name);
 
-                                                //     $imgname = $file_name.'.png';
-                                                //     $data = 'https://datacenter.pa-bojonegoro.go.id:8089/keaslian/akta/asli/' . $hasil['kode'];
-                                                //     $logo = base_url().'assets/img/logo.png';
-                                                //     $sdir = explode("/", $_SERVER['REQUEST_URI']);
-                                                //     $dir = str_replace($sdir[count($sdir) - 1], "", $_SERVER['REQUEST_URI']);
-
-                                                //     // === Create qrcode image
-                                                //     // include('phpqrcode/qrlib.php');
-                                                //     QRcode::png($data, $imgname, QR_ECLEVEL_L, 11.45, 0);
-
-                                                //     // === Adding image to qrcode
-                                                //     $QR = imagecreatefrompng($imgname);
-                                                //     if ($logo !== FALSE) {
-                                                //         $logopng = imagecreatefrompng($logo);
-                                                //         $QR_width = imagesx($QR);
-                                                //         $QR_height = imagesy($QR);
-                                                //         $logo_width = imagesx($logopng);
-                                                //         $logo_height = imagesy($logopng);
-
-                                                //         list($newwidth, $newheight) = getimagesize($logo);
-                                                //         $out = imagecreatetruecolor($QR_width, $QR_width);
-                                                //         imagecopyresampled($out, $QR, 0, 0, 0, 0, $QR_width, $QR_height, $QR_width, $QR_height);
-                                                //         imagecopyresampled($out, $logopng, $QR_width / 2.65, $QR_height / 2.65, 0, 0, $QR_width / 4, $QR_height / 4, $newwidth, $newheight);
-                                                //     }
-                                                //     imagepng($out, $imgname);
-                                                //     imagedestroy($out);
-
-                                                //     // === Change image color
-                                                //     $im = imagecreatefrompng($imgname);
-                                                //     $r = 44;
-                                                //     $g = 62;
-                                                //     $b = 80;
-                                                //     for ($x = 0; $x < imagesx($im); ++$x) {
-                                                //         for ($y = 0; $y < imagesy($im); ++$y) {
-                                                //             $index     = imagecolorat($im, $x, $y);
-                                                //             $c       = imagecolorsforindex($im, $index);
-                                                //             if (($c['red'] < 100) && ($c['green'] < 100) && ($c['blue'] < 100)) { // dark colors
-                                                //                 // here we use the new color, but the original alpha channel
-                                                //                 $colorB = imagecolorallocatealpha($im, 0x12, 0x2E, 0x31, $c['alpha']);
-                                                //                 imagesetpixel($im, $x, $y, $colorB);
-                                                //             }
-                                                //         }
-                                                //     }
-                                                //     imagepng($im, $imgname);
-                                                //     imagedestroy($im);
-
-                                                //     // === Convert Image to base64
-                                                //     $type = pathinfo($imgname, PATHINFO_EXTENSION);
-                                                //     $data = file_get_contents($imgname);
-                                                //     $imgbase64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
-
-                                                //     // === Show image
-                                                //     // echo "<img src='$imgbase64' style='position:relative;display:block;width:240px;height:240px;margin:160px auto;'>";
+                                                //     $params['data'] = 'https://datacenter.pa-bojonegoro.go.id:8089/keaslian/akta/asli/' . $hasil['kode'];
+                                                //     $params['level'] = 'H';
+                                                //     $params['size'] = 10;
+                                                //     $params['savename'] = FCPATH . $file_name . '.png';
+                                                //     $this->ciqrcode->generate($params);
                                                 // }
+                                                ?>
+                                             <?php
+                                                if ($hasil['kode'] != NULL) {
+                                                    $file_name = str_replace('/', '_', $hasil['nomor_perkara']);
+                                                    $file_name = str_replace('.', '', $file_name);
+
+                                                    $imgname = $file_name.'.png';
+                                                    $data = 'https://datacenter.pa-bojonegoro.go.id:8089/keaslian/akta/asli/' . $hasil['kode'];
+                                                    $logo = base_url().'assets/img/logo.png';
+                                                    $sdir = explode("/", $_SERVER['REQUEST_URI']);
+                                                    $dir = str_replace($sdir[count($sdir) - 1], "", $_SERVER['REQUEST_URI']);
+
+                                                    // === Create qrcode image
+                                                    // include('phpqrcode/qrlib.php');
+                                                    QRcode::png($data, $imgname, QR_ECLEVEL_H, 11.45, 2);
+
+                                                    // === Adding image to qrcode
+                                                    $QR = imagecreatefrompng($imgname);
+                                                    if ($logo !== FALSE) {
+                                                        $logopng = imagecreatefrompng($logo);
+                                                        $QR_width = imagesx($QR);
+                                                        $QR_height = imagesy($QR);
+                                                        $logo_width = imagesx($logopng);
+                                                        $logo_height = imagesy($logopng);
+
+                                                        list($newwidth, $newheight) = getimagesize($logo);
+                                                        $out = imagecreatetruecolor($QR_width, $QR_width);
+                                                        imagecopyresampled($out, $QR, 0, 0, 0, 0, $QR_width, $QR_height, $QR_width, $QR_height);
+                                                        imagecopyresampled($out, $logopng, $QR_width / 2.65, $QR_height / 2.65, 0, 0, $QR_width / 4, $QR_height / 4, $newwidth, $newheight);
+                                                    }
+                                                    imagepng($out, $imgname);
+                                                    imagedestroy($out);
+
+                                                    // === Change image color
+                                                    $im = imagecreatefrompng($imgname);
+                                                    $r = 44;
+                                                    $g = 62;
+                                                    $b = 80;
+                                                    for ($x = 0; $x < imagesx($im); ++$x) {
+                                                        for ($y = 0; $y < imagesy($im); ++$y) {
+                                                            $index     = imagecolorat($im, $x, $y);
+                                                            $c       = imagecolorsforindex($im, $index);
+                                                            if (($c['red'] < 100) && ($c['green'] < 100) && ($c['blue'] < 100)) { // dark colors
+                                                                // here we use the new color, but the original alpha channel
+                                                                $colorB = imagecolorallocatealpha($im, 0x12, 0x2E, 0x31, $c['alpha']);
+                                                                imagesetpixel($im, $x, $y, $colorB);
+                                                            }
+                                                        }
+                                                    }
+                                                    imagepng($im, $imgname);
+                                                    imagedestroy($im);
+
+                                                    // === Convert Image to base64
+                                                    $type = pathinfo($imgname, PATHINFO_EXTENSION);
+                                                    $data = file_get_contents($imgname);
+                                                    $imgbase64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+
+                                                    // === Show image
+                                                    // echo "<img src='$imgbase64' style='position:relative;display:block;width:240px;height:240px;margin:160px auto;'>";
+                                                }
                                                 ?>
                                              <tr>
                                                  <td width="20px"><?php echo $no++ ?></td>
